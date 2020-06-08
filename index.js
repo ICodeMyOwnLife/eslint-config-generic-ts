ebb016e4fab0f28d343c386169ae90351e411de4
HienTM <hientm@sendo.vn>
2020-06-08 11:01:48 +0700

    fix: ignore @typescript-eslint/explicit-module-boundary-types rule

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ module.exports = {
   },
   rules: {
     "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-empty-interface": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
